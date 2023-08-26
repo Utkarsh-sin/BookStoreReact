ea7c90f6bb5dd7671d6e0f3d4ff5330ed02f1452
Utkarsh-sin <sinhautkarsh38@gmail.com>
2023-08-26 23:30:16 +0530

    sol

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BookStore from './Pages/BookStore';
+
+function App() {
+  return (
+    <div className="App">
+      <BookStore/>
+    </div>
+  );
+}
+
+export default App;
